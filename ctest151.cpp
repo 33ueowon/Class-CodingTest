5245c9eb9cfffdb3d163a23f5cec9a3683bfee00
이여원 <127466476+33ueowon@users.noreply.github.com>
2024-06-27 11:15:57 +0900

    06.27(151)

--- ctest151.cpp
+++ ctest151.cpp
@@ -1,21 +1,13 @@
 #include <stdio.h>
+#include <math.h>
 int main(){
-	int jumsu,p;
-	printf("jumsu(1~100) ===> ");
-	scanf("%d", &jumsu);
-	p = jumsu / 10;
-	switch(p){
-		case 10: printf("A");
-				break;
-		case 9: printf("A");
-				break;
-		case 8: printf("B");
-				break;
-		case 7: printf("C");
-				break;
-		case 6: printf("D");
-				break;
-		default: printf("E");
-	}
+	int a,b;
+	long long int r;
+	printf("a ===> ");
+	scanf("%d", &a);
+	printf("b ===> ");
+	scanf("%d", &b);
+	r = pow(a,b);
+	printf("RESULT ===> %lld" , r);
 	
 }
